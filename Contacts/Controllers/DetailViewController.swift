bab9f46c933fb6c476638d4b547eadd438844e91
Airels <yohan13240@hotmail.fr>
2023-03-17 11:48:45 +0100

    safe delete

--- Contacts/Controllers/DetailViewController.swift
+++ Contacts/Controllers/DetailViewController.swift
@@ -26,36 +26,38 @@ class DetailViewController: UIViewController {
     }
     
     @IBAction func onDelete(_ sender: Any) {
-        let alert = UIAlertController(title: "Suppression contact", message: "Voulez-vous vraiment supprimer ce contact ?", preferredStyle: UIAlertController.Style.alert)
-        
-        alert.addAction(UIAlertAction(title: "Oui", style: .destructive, handler: { action in
-            let config = URLSessionConfiguration.default
-            config.allowsCellularAccess = true
-            let session = URLSession(configuration: config)
-            let url = URL(string: "https://contacts.education.craftedbits.io/contacts")!
-            var req = URLRequest(url: url)
-            req.httpMethod = "DELETE"
-            req.addValue("Guillaume", forHTTPHeaderField: "Contacts-Owner")
-            req.url?.appendPathComponent("\(self.contact?.id)")
+        if let contactID = contact?.id {
+            let alert = UIAlertController(title: "Suppression contact", message: "Voulez-vous vraiment supprimer ce contact ?", preferredStyle: UIAlertController.Style.alert)
             
-            let task = URLSession.shared.dataTask(with: req) { (data, res, err) in
-                let httpResponse = res as! HTTPURLResponse
+            alert.addAction(UIAlertAction(title: "Oui", style: .destructive, handler: { action in
+                let config = URLSessionConfiguration.default
+                config.allowsCellularAccess = true
+                let session = URLSession(configuration: config)
+                let url = URL(string: "https://contacts.education.craftedbits.io/contacts")!
+                var req = URLRequest(url: url)
+                req.httpMethod = "DELETE"
+                req.addValue("Guillaume", forHTTPHeaderField: "Contacts-Owner")
+                req.url?.appendPathComponent("\(contactID)")
                 
-                if (httpResponse.statusCode == 200) {
-                    DispatchQueue.main.async {
-                        self.navigationController?.popViewController(animated: true)
+                let task = URLSession.shared.dataTask(with: req) { (data, res, err) in
+                    let httpResponse = res as! HTTPURLResponse
+                    
+                    if (httpResponse.statusCode == 200) {
+                        DispatchQueue.main.async {
+                            self.navigationController?.popViewController(animated: true)
+                        }
+                    } else {
+                        print("\(httpResponse.statusCode) Error")
                     }
-                } else {
-                    print("\(httpResponse.statusCode) Error")
                 }
-            }
+                
+                task.resume()
+            }))
             
-            task.resume()
-        }))
-        
-        alert.addAction(UIAlertAction(title: "Non", style: .default))
-        
-        self.present(alert, animated: true)
+            alert.addAction(UIAlertAction(title: "Non", style: .default))
+            
+            self.present(alert, animated: true)
+        }
     }
     
     /*
